1fffa8c010a832838be2659b20121b82b8b4e5e0
Yuanuox <yuanuox@outlook.com>
2021-01-20 22:16:53 +0800

    re implement preferences configuration UI.

--- src/main/java/org/appxi/cbeta/explorer/prefs/PreferencesController.java
+++ src/main/java/org/appxi/cbeta/explorer/prefs/PreferencesController.java
@@ -2,26 +2,22 @@ package org.appxi.cbeta.explorer.prefs;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import org.appxi.cbeta.explorer.workbench.WorkbenchOpenpartControllerExt;
+import org.appxi.javafx.control.DialogPaneEx;
 import org.appxi.javafx.theme.Theme;
+import org.appxi.javafx.workbench.views.WorkbenchWorktoolController;
 import org.appxi.prefs.UserPrefs;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class PreferencesController extends WorkbenchOpenpartControllerExt {
-    private final ToggleGroup configThemeGroup = new ToggleGroup();
-
+public class PreferencesController extends WorkbenchWorktoolController {
     public PreferencesController() {
         super("PREFERENCES", "偏好设置");
     }
@@ -34,8 +30,8 @@ public class PreferencesController extends WorkbenchOpenpartControllerExt {
     }
 
     @Override
-    public boolean isWorktoolSupport() {
-        return true;
+    public Node getViewport() {
+        return null;
     }
 
     @Override
@@ -44,44 +40,61 @@ public class PreferencesController extends WorkbenchOpenpartControllerExt {
 
     @Override
     public void onViewportSelected(boolean firstTime) {
-        if (firstTime) {
-            final List<Node> nodes = new ArrayList<>();
+        final Alert alert = new Alert(Alert.AlertType.NONE);
+        alert.setTitle(viewName);
 
-            // init ui
-            nodes.add(new Label("主题/颜色"));
-            final Theme currTheme = this.getThemeProvider().getTheme();
-            configThemeGroup.setUserData(currTheme);
-            getThemeProvider().getThemes().forEach(t -> {
-                final RadioButton btn = new RadioButton(t.title);
-                btn.setToggleGroup(configThemeGroup);
-                btn.setUserData(t);
+        final List<Node> nodes = new ArrayList<>();
 
-                final FontAwesomeIconView faicon = new FontAwesomeIconView(FontAwesomeIcon.CIRCLE);
-                faicon.setFill(Color.valueOf(t.accentColor));
-                btn.setGraphic(faicon);
+        buildThemeConfig(nodes);
 
-                btn.setOnAction(this::handleConfigThemeSelect);
-                nodes.add(btn);
-                //
-                if (Objects.equals(currTheme, t))
-                    btn.setSelected(true);
-            });
+        final DialogPaneEx pane = new DialogPaneEx();
+        pane.setStyle("-fx-padding: 1em;");
+        pane.setContent(new VBox(nodes.toArray(new Node[0])));
+        pane.setPrefSize(300, 400);
+        pane.setMinSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        pane.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
 
-            // update ui
-            this.viewpartVbox.getChildren().addAll(nodes);
-        }
+        alert.setDialogPane(pane);
+
+        showAlertWithThemeAndWaitForNothing(alert);
     }
 
-    @FXML
-    void handleConfigThemeSelect(ActionEvent event) {
-        final ToggleButton themeBtn = (ToggleButton) event.getSource();
-        final Theme currTheme = (Theme) themeBtn.getUserData();
-        final Theme lastTheme = (Theme) configThemeGroup.getUserData();
-        if (currTheme == lastTheme)
-            return;
+    private void buildThemeConfig(List<Node> nodes) {
+        if (nodes.size() > 0)
+            nodes.add(createSeparator());
+        //
+        nodes.add(new Label("主题/颜色"));
 
-        configThemeGroup.setUserData(currTheme);
-        this.getThemeProvider().applyTheme(currTheme);
-        UserPrefs.prefs.setProperty("ui.theme", currTheme.name);
+        final Theme currentTheme = this.getThemeProvider().getTheme();
+        final ToggleGroup themeBtnGroup = new ToggleGroup();
+        getThemeProvider().getThemes().forEach(t -> {
+            final RadioButton btn = new RadioButton(t.title);
+            btn.setToggleGroup(themeBtnGroup);
+            btn.setUserData(t);
+
+            final FontAwesomeIconView faicon = new FontAwesomeIconView(FontAwesomeIcon.CIRCLE);
+            faicon.setFill(Color.valueOf(t.accentColor));
+            btn.setGraphic(faicon);
+
+            btn.setStyle("-fx-padding: .5em 1em;");
+
+            nodes.add(btn);
+            //
+            if (Objects.equals(currentTheme, t))
+                btn.setSelected(true);
+        });
+        themeBtnGroup.selectedToggleProperty().addListener(((val, ov, nv) -> {
+            if (ov == nv)
+                return;
+            final Theme selTheme = (Theme) nv.getUserData();
+            this.getThemeProvider().applyTheme(selTheme);
+            UserPrefs.prefs.setProperty("ui.theme", selTheme.name);
+        }));
+    }
+
+    private Separator createSeparator() {
+        final Separator result = new Separator();
+        result.setStyle("-fx-padding: .5em 0;");
+        return result;
     }
 }
