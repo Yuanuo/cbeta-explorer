bdb227157eb10152424000b448055cb0ce5b09a6
Yuanuox <yuanuox@outlook.com>
2021-01-23 13:35:03 +0800

    bump version to 21.01.23

--- src/main/java/org/appxi/cbeta/explorer/AppInfo.java
+++ src/main/java/org/appxi/cbeta/explorer/AppInfo.java
@@ -4,7 +4,7 @@ public abstract class AppInfo {
     public static final String ID = "cbetaExplorer";
     public static final String NAME = "智悲乐藏";
 
-    public static final String VERSION = "21.01.00";
+    public static final String VERSION = "21.01.23";
 
     private AppInfo() {
     }
