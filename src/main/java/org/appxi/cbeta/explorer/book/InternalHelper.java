d75e9c753016686f4fb63d5374a88b525074bcde
Yuanuox <yuanuox@outlook.com>
2021-01-30 10:29:25 +0800

    确保每个js/css都能被引入到html中

--- src/main/java/org/appxi/cbeta/explorer/book/InternalHelper.java
+++ src/main/java/org/appxi/cbeta/explorer/book/InternalHelper.java
@@ -6,27 +6,29 @@ import org.appxi.util.FileHelper;
 import org.appxi.util.StringHelper;
 
 import java.nio.file.Path;
-import java.util.List;
 
 class InternalHelper {
-    private static final String[] includeNames = {"jquery.0.js",
+    private static final String[] htmlIncludeNames = {"jquery.0.js",
             "jquery.isinviewport.js",
             "jquery.highlight.js",
             "jquery.scrollto.js",
             "jquery.finder.js",
             "app.css",
             "app.js"};
-    static String[] htmlIncludes;
+    static final String[] htmlIncludesPaths = new String[htmlIncludeNames.length];
 
     static void initHtmlIncludes() {
-        final List<Path> files = FileHelper.extractFiles(
+        FileHelper.extractFiles(
                 file -> InternalHelper.class.getResourceAsStream(StringHelper.concat("/appxi/cbetaExplorer/incl/", file)),
-                file -> UserPrefs.dataDir().resolve(".html-incl").resolve(StringHelper.concat(AppInfo.VERSION, "-", file)),
-                includeNames);
-        htmlIncludes = new String[files.size()];
-        for (int i = 0; i < files.size(); i++) {
-            htmlIncludes[i] = files.get(i).toUri().toString();
+                InternalHelper::buildHtmlIncludeTarget,
+                htmlIncludeNames);
+        for (int i = 0; i < htmlIncludeNames.length; i++) {
+            htmlIncludesPaths[i] = buildHtmlIncludeTarget(htmlIncludeNames[i]).toUri().toString();
         }
+    }
+
+    private static Path buildHtmlIncludeTarget(String file) {
+        return UserPrefs.dataDir().resolve(".html-incl").resolve(StringHelper.concat(AppInfo.VERSION, "-", file));
     }
 
 }
