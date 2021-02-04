8d3b69171f194c485cd99fe5ff545761c6062efc
Yuanuox <yuanuox@outlook.com>
2021-02-04 20:45:40 +0800

    补充提交

---
+++ web-incl/jquery.ext.js
@@ -0,0 +1,47 @@
+jQuery.fn.tagName = function () {
+    return this.prop("tagName").toLowerCase();
+};
+jQuery.fn.cssSelector = function () {
+    if (this.is("[id]"))
+        return this.tagName() + '#' + this.attr('id');
+    const paths = [];
+    this.each(function (index, element) {
+        let path, $node = jQuery(element);
+        while ($node.length) {
+            let realNode = $node.get(0), name = realNode.localName;
+            if (!name) {
+                break;
+            }
+            name = name.toLowerCase();
+            const parent = $node.parent();
+            const sameTagSiblings = parent.children(name);
+
+            if (sameTagSiblings.length > 1) {
+                const allSiblings = parent.children();
+                const index = allSiblings.index(realNode) + 1;
+                if (index > 0) {
+                    name += ':nth-child(' + index + ')';
+                }
+            }
+            path = name + (path ? ' > ' + path : '');
+            $node = parent;
+        }
+        paths.push(path);
+    });
+    return paths.join(',');
+};
+jQuery.fn.traverse = function (predicate) {
+    if (predicate(this[0]))
+        return true;
+    const list = this.contents();
+    for (let i = 0; i < list.length; i++) {
+        const node = list[i];
+        if (node.nodeType === 3) {
+            if (predicate(node))
+                return true;
+        } else if ($(node).traverse(predicate)) {
+            return true;
+        }
+    }
+    return false;
+};
