de1ca6b73aade83f9a9cc8d547d648b8792e3618
Yuanuox <yuanuox@outlook.com>
2021-02-08 15:03:42 +0800

    引入数据库需要添加的Module信息

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,8 +15,15 @@ module appxi.cbetaExplorer {
     requires org.jsoup;
     requires de.jensd.fx.fontawesomefx.materialicons;
 
+    requires java.sql;
+    requires static com.h2database;
+    requires static ormlite.core;
+    requires static ormlite.jdbc;
+    requires static org.json;
+
     exports org.appxi.cbeta.explorer;
     exports org.appxi.cbeta.explorer.bak;
 
+    opens org.appxi.cbeta.explorer.dao;// to ormlite.core
     opens org.appxi.cbeta.explorer.book;
 }
