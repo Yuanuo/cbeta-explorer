7aa2d0a4237a448cfa2fad825e4a5c051a223237
Yuanuox <yuanuox@outlook.com>
2021-02-12 13:35:43 +0800

    移除未使用的代码

--- src/main/java/org/appxi/cbeta/explorer/WorkbenchRootController.java
+++ src/main/java/org/appxi/cbeta/explorer/WorkbenchRootController.java
@@ -3,13 +3,9 @@ package org.appxi.cbeta.explorer;
 import org.appxi.cbeta.explorer.book.BookDataPlaceController;
 import org.appxi.cbeta.explorer.book.BookListController;
 import org.appxi.cbeta.explorer.home.AboutController;
-import org.appxi.cbeta.explorer.home.MaterialIconsController;
 import org.appxi.cbeta.explorer.prefs.PreferencesController;
 import org.appxi.cbeta.explorer.recent.RecentController;
-import org.appxi.cbeta.explorer.search.FullTextController;
-import org.appxi.cbeta.explorer.todo.TodoListController;
 import org.appxi.cbeta.explorer.widget.WidgetsController;
-import org.appxi.javafx.helper.FxHelper;
 import org.appxi.javafx.workbench.WorkbenchApplication;
 import org.appxi.javafx.workbench.WorkbenchPrimaryController;
 import org.appxi.javafx.workbench.WorkbenchViewController;
@@ -30,16 +26,8 @@ class WorkbenchRootController extends WorkbenchPrimaryController {
         result.add(new BookDataPlaceController(getApplication()));
         result.add(new RecentController(getApplication()));
 
-        if (!FxHelper.productionMode) {
-            result.add(new TodoListController(getApplication()));
-            result.add(new FullTextController(getApplication()));
-        }
-
         result.add(new WidgetsController(getApplication()));
 
-        // 开发调试模式中用于查找可用图标的工具，在实际运行时不启用
-        if (!FxHelper.productionMode)
-            result.add(new MaterialIconsController(getApplication()));
         result.add(new PreferencesController(getApplication()));
         result.add(new AboutController(getApplication()));
         return result;
