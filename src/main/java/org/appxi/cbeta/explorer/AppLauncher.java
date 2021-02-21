e23c945aa3acc24f59a81cfad8001bb68cff7f90
Yuanuox <yuanuox@outlook.com>
2021-02-21 20:33:25 +0800

    设定默认语言为zh_CN，否则文本框等右键菜单将显示英文

--- src/main/java/org/appxi/cbeta/explorer/AppLauncher.java
+++ src/main/java/org/appxi/cbeta/explorer/AppLauncher.java
@@ -2,9 +2,12 @@ package org.appxi.cbeta.explorer;
 
 import javafx.application.Application;
 
+import java.util.Locale;
+
 public class AppLauncher {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.SIMPLIFIED_CHINESE);
         System.setProperty("javafx.preloader", "org.appxi.cbeta.explorer.AppPreloader");
         try {
             Application.launch(AppWorkbench.class, args);
