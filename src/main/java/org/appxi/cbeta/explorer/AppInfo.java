3726ffca0e048bfe05236f85571e7c6c6cff7e6d
Yuanuox <yuanuox@outlook.com>
2021-03-06 17:55:38 +0800

    发布版本21.02.06

--- src/main/java/org/appxi/cbeta/explorer/AppInfo.java
+++ src/main/java/org/appxi/cbeta/explorer/AppInfo.java
@@ -4,7 +4,7 @@ public abstract class AppInfo {
     public static final String ID = "cbetaExplorer";
     public static final String NAME = "智悲乐藏";
 
-    public static final String VERSION = "21.03.04";
+    public static final String VERSION = "21.03.06";
 
     private AppInfo() {
     }
