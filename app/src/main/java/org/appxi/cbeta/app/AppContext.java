57be7af532c8f0b14d5a0fcf3da747637ae44c42
Yuanuox <yuanuox@outlook.com>
2022-07-01 22:48:13 +0800

    新增切换数据源选项

--- app/src/main/java/org/appxi/cbeta/app/AppContext.java
+++ app/src/main/java/org/appxi/cbeta/app/AppContext.java
@@ -2,14 +2,23 @@ package org.appxi.cbeta.app;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.input.DataFormat;
+import javafx.stage.FileChooser;
 import org.appxi.cbeta.BookMap;
 import org.appxi.cbeta.Bookcase;
+import org.appxi.cbeta.BookcaseInZip;
 import org.appxi.cbeta.TripitakaMap;
 import org.appxi.cbeta.app.dao.DaoHelper;
 import org.appxi.cbeta.app.dao.DaoService;
 import org.appxi.cbeta.app.event.GenericEvent;
+import org.appxi.javafx.settings.DefaultOption;
 import org.appxi.javafx.settings.DefaultOptions;
+import org.appxi.javafx.settings.OptionEditorBase;
 import org.appxi.javafx.settings.SettingsList;
 import org.appxi.prefs.Preferences;
 import org.appxi.prefs.PreferencesInMemory;
@@ -24,6 +33,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -126,6 +136,56 @@ public abstract class AppContext {
             return new DefaultOptions<HanLang>("简繁体", "以 简体/繁体 显示经名标题、阅读视图等经藏数据", "VIEWER", true)
                     .setValues(HanLang.hans, HanLang.hant, HanLang.hantHK, HanLang.hantTW)
                     .setValueProperty(valueProperty);
+        });
+        //
+        SettingsList.add(() -> {
+            Preferences prefsG = new PreferencesInProperties(
+                    Path.of(System.getProperty("user.home")).resolve(AppLauncher.dataDirName + "/.config/.prefs"));
+            return new DefaultOption<String>("切换数据源", null, "***数据源", true,
+                    option -> new OptionEditorBase<>(option, new Button()) {
+                        private StringProperty valueProperty;
+
+                        @Override
+                        public StringProperty valueProperty() {
+                            if (this.valueProperty == null) {
+                                this.valueProperty = new SimpleStringProperty();
+                                getEditor().setText("选择数据包");
+                                getEditor().setOnAction(evt -> {
+                                    final FileChooser chooser = new FileChooser();
+                                    chooser.setTitle("请选择CBETA Bookcase Zip数据包");
+                                    chooser.getExtensionFilters().add(
+                                            new FileChooser.ExtensionFilter("CBETA Bookcase Zip File", "cbeta.zip", "bookcase.zip", "bookcase_*.zip")
+                                    );
+                                    final File selected = chooser.showOpenDialog(App.app().getPrimaryStage());
+                                    if (null == selected) {
+                                        return;
+                                    }
+                                    try {
+                                        String path = selected.getAbsolutePath();
+                                        // 验证数据源有效性
+                                        new BookcaseInZip(path);
+                                        // 数据源有效，更新记录
+                                        prefsG.setProperty("bookcase", path);
+                                        prefsG.save();
+                                        setValue(path);
+
+                                        final Alert alert = new Alert(Alert.AlertType.INFORMATION, "已切换数据源，下次启动时生效！");
+                                        alert.setHeaderText(null);
+                                        alert.initOwner(app.getPrimaryStage());
+                                        alert.show();
+                                    } catch (Throwable ignore) {
+                                        App.app().toastError("验证失败，请选择有效的数据源！");
+                                    }
+                                });
+                            }
+                            return this.valueProperty;
+                        }
+
+                        @Override
+                        public void setValue(String value) {
+                            getEditor().setTooltip(new Tooltip("当前数据源：" + value));
+                        }
+                    }).setValue(prefsG.getString("bookcase", ""));
         });
     }
 
