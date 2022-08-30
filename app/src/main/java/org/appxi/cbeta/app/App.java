70f5e5e97773c3f39a9ec4e1d16b9bcd132f7d3d
Yuanuox <yuanuox@outlook.com>
2022-08-30 19:48:05 +0800

    发布版本22.08.30

--- app/src/main/java/org/appxi/cbeta/app/App.java
+++ app/src/main/java/org/appxi/cbeta/app/App.java
@@ -34,7 +34,7 @@ import java.util.stream.Stream;
 public class App extends WorkbenchApp {
     public static final String ID = "cbetaExplorer";
     public static final String NAME = "智悲乐藏";
-    public static final String VERSION = "22.08.03";
+    public static final String VERSION = "22.08.30";
     private static App instance;
 
     public App() {
