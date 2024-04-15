50dae5036a850027a65b1dae42c3536cfa733e55
Yuanuox <yuanuox@outlook.com>
2024-04-15 19:21:23 +0800

    更新版本号

--- app/src/main/java/org/appxi/cbeta/app/App.java
+++ app/src/main/java/org/appxi/cbeta/app/App.java
@@ -36,7 +36,7 @@ import java.util.stream.Stream;
 public class App extends BaseApp2 {
     public static final String ID = "cbetaExplorer";
     public static final String NAME = "典 · 集";
-    public static final String VERSION = "24.03.25";
+    public static final String VERSION = "24.04.15";
 
     public final Bookcase bookcase;
 
